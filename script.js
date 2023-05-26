14f61b559206453fc8d61244a9c407db241bdab2
Koundu <121346044+Koundu@users.noreply.github.com>
2023-05-26 17:10:54 +0530

    created a submission

--- script.js
+++ script.js
@@ -4,6 +4,6 @@ function palindrome(str){
 const temp1 = str.split("")
 temp1.reverse();
 const res = temp1.join("");
-return console.log(str===res);
+console.log(str===res);
 }
 module.exports = palindrome
